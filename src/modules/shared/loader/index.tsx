87323174787e139b5a5a77822a8fdc9584daeac1
Hery Vandoro <vandorohery99@gmail.com>
2019-10-17 16:39:03 +0700

    enhance(ui): set loader image

--- src/modules/shared/loader/index.tsx
+++ src/modules/shared/loader/index.tsx
@@ -10,55 +10,11 @@ export default class ReboxLoader extends Component<ReboxLoaderProps, any> {
 		const { transparent } = this.props;
 		return (
 			<div
-				className={`diamond-loader ${
+				className={`loader ${
 					transparent ? 'transparent' : ''
 				}`}>
-				<span>
-					<svg
-						version="1.1"
-						id="Capa_1"
-						xmlns="http://www.w3.org/2000/svg"
-						x="0px"
-						y="0px"
-						width="60"
-						height="25"
-						viewBox="0 0 58 58">
-						<polygon
-							style={{ fill: '#CC2E48' }}
-							points="29,55 0,19 58,19 "
-						/>
-						<polygon
-							style={{ fill: '#FC3952' }}
-							points="58,19 0,19 10,3 48,3 "
-						/>
-						<polygon
-							style={{ fill: '#F76363' }}
-							points="42.154,19 48,3 10,3 15.846,19 "
-						/>
-						<polygon
-							style={{ fill: '#F49A9A' }}
-							points="42,19 29,3 16,19 "
-						/>
-						<polygon
-							style={{ fill: '#CB465F' }}
-							points="15.846,19 29,55 42.154,19 "
-						/>
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-						<g />
-					</svg>
+				<span className="logo">
+					<img width="60%" src="/images/loader.png" />
 				</span>
 			</div>
 		);
